e666105c30d2bede54d1b85dfb67486ec5e5ec83
matheusfdosan <matheusfaustinoe20@gmail.com>
2025-01-26 22:17:09 -0300

    link on about section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,15 +2,33 @@ import React from "react"
 import "./styles.css"
 
 function About() {
-  return <div id="about">
-    <div className="container">
-
-    <h2>Sobre mim</h2>
-    <div className="line"></div>
-    <p>Sou Matheus Faustino, técnico em Informática com foco em Programação pelo CEAP (Centro Educacional Assistencial Profissionalizante) e 5 anos de experiência com desenvolvimento web. Gosto desenvolver tecnologias que possam fazer a diferença.</p>
-    <p>Durante a minha trajetória, desenvolvi habilidades com HTML, CSS, JavaScript, React.js, Bancos de dados (MongoDB, MySQL e PostgreSQL), Java e Python. Um dos destaques do meu percurso foi minha participação na FeCEAP, com o projeto <span>MelhorCidade</span>, que recebeu excelente avaliação dos técnicos responsáveis. Atualmente, estou expandindo meu conhecimento para outras áreas como desenvolvimento Mobile com Java e React Native, enquanto aprofundo minhas habilidade em Back-end.</p>
+  return (
+    <div id="about">
+      <div className="container">
+        <h2>Sobre mim</h2>
+        <div className="line"></div>
+        <p>
+          Sou Matheus Faustino, técnico em Informática com foco em Programação
+          pelo CEAP (Centro Educacional Assistencial Profissionalizante) e 5
+          anos de experiência com desenvolvimento web. Gosto desenvolver
+          tecnologias que possam fazer a diferença.
+        </p>
+        <p>
+          Durante a minha trajetória, desenvolvi habilidades com HTML, CSS,
+          JavaScript, React.js, Bancos de dados (MongoDB, MySQL e PostgreSQL),
+          Java e Python. Um dos destaques do meu percurso foi minha participação
+          na FeCEAP, com o projeto{" "}
+          <span onClick={() => window.open("https://melhorcidade.vercel.app/")}>
+            MelhorCidade
+          </span>
+          , que recebeu excelente avaliação dos técnicos responsáveis.
+          Atualmente, estou expandindo meu conhecimento para outras áreas como
+          desenvolvimento Mobile com Java e React Native, enquanto aprofundo
+          minhas habilidade em Back-end.
+        </p>
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default About
